32e7986a1dda90f1c081684b704807494dc8c479
Alberto Manzano <alberto.manzano.herrero@gmail.com>
2022-03-15 09:23:40 +0000

    Middle step with maximum likelihood

--- libraries/AE/maximum_likelihood_ae.py
+++ libraries/AE/maximum_likelihood_ae.py
@@ -23,79 +23,13 @@ import copy
 import numpy as np
 import pandas as pd
 import scipy.optimize as so
-from qat.core import Batch
 
-from amplitude_amplification import grover
+import qat.lang.AQASM as qlm 
+from qat.qpus import get_default_qpu
 
+from AE.amplitude_amplification import grover
+from data_extracting import get_results
 
-def apply_gate(q_prog, q_gate, m_k, nbshots=0):
-    """
-    Apply the self.q_gate to the circuit a input number of times
-    This method creates a quantum program that applies the
-    Q operator n_ops times on the circuit where the probability
-    and the function were loaded
-
-    Parameters
-    ----------
-    q_gate : QLM gate
-        QLM gate with the Groover-like operator to be applied on the q_prog
-    m_k : int
-        number of times to apply the q_gate to the q_prog
-    nbshots : int
-        number of shots to perform by the QLM solver
-
-    Returns
-    ----------
-
-    pdf : pandas DataFrame
-        results of the measurement of the last qbit
-    circuit : QLM circuit object
-        circuit object generated for the quantum program
-    job : QLM job object
-        job object generated for the quantum circuit
-    """
-    prog_q = copy.deepcopy(q_prog)
-    q_bits = prog_q.registers[0]
-    #for _ in range(m_k):
-    #    prog_q.apply(q_gate, q_bits)
-    step_q_gate = load_qn_gate(q_gate, m_k)
-    prog_q.apply(step_q_gate, q_bits)
-    #circuit = prog_q.to_circ(submatrices_only=True)
-    circuit = create_circuit(prog_q)
-    #job = circuit.to_job(qubits=[len(q_bits)-1], nbshots=nbshots)
-    job = create_job(circuit, shots=nbshots, qubits=[len(q_bits)-1])
-    return circuit, job
-
-
-def get_probabilities(InputPDF):
-    """
-    Auxiliar function for changing the presentation of the results for an
-    input pandas DataFrame
-
-    Parameters
-    ----------
-
-    InputPDF : pandas DataFrame.
-        DataFrame with the info of the measurments. Should have following
-        columns:
-        States : states for the qbit measurement
-        Probability : probability for each state
-
-    Returns
-    ----------
-
-    output_pdf : pandas DataFrame
-        Changes the presentation of the results of a mesurement.
-        Columns are now the  probability of the different states.
-    """
-
-    pdf = InputPDF.copy(deep=True)
-    columns = ['Probability_{}'.format(i) for i in pdf['States']]
-    output_pdf = pd.DataFrame(
-        pdf['Probability'].values.reshape(1, len(pdf)),
-        columns=columns
-    )
-    return output_pdf
 
 def likelihood(theta, m_k, h_k, n_k):
     """
@@ -143,7 +77,7 @@ class MLAE:
     algorithm
     """
 
-    def __init__(self, **kwargs):
+    def __init__(self,oracle: qlm.QRoutine,target: list,index: list,**kwargs):
         """
 
         Method for initializing the class
@@ -178,35 +112,26 @@ class MLAE:
                 QLM Program withe the initial Psi state over the
                 Grover-like operator will be applied
                 Only used if oracle is None
-            grover : QLM gate or routine
-                Grover-like operator which autovalues want to be calculated
-                Only used if oracle is None
         """
         #Setting attributes
-        self.oracle = kwargs.get('oracle', None)
-        if self.oracle is not None:
-            #Creates QLM program from base gate
-            self.q_prog = create_qprogram(self.oracle)
-            #Creates the Grover-like operator from oracle
-            self.q_gate = load_q_gate(self.oracle)
-        else:
-            #In this case we load directly the initial state
-            #and the grover operator
-            self.q_prog = kwargs.get('initial_state', None)
-            self.q_gate = kwargs.get('grover', None)
-            if (self.q_prog is None) or (self.q_gate is None):
-                text = """If oracle was not provided initial_state and grover
-                keys should be provided"""
-                raise KeyError(text)
+        self.oracle = copy.deepcopy(oracle)
+        self.target = target
+        self.index = index
+            
+        #Creates the Grover-like operator from oracle
+        self.grover_oracle = grover(self.oracle,target,index)
+
 
         #A complete list of m_k
         self.list_of_mks_ = kwargs.get('list_of_mks', 10)
         self.list_of_mks = self.list_of_mks_
 
         #If 0 we compute the exact probabilities
-        self.nbshots = kwargs.get('nbshots', 0)
+        self.shots = kwargs.get('nbshots', 0)
         #Set the QPU to use
-        self.linalg_qpu = kwargs.get('qpu', get_qpu())
+        self.linalg_qpu = kwargs.get('qpu')
+        if (self.linalg_qpu is None):
+            self.linalg_qpu = get_default_qpu
         ##delta for avoid problems in 0 and pi/2 theta limits
         self.delta = kwargs.get('delta', 1.0e-5)
         #This is the default number of shots used for computing
@@ -221,11 +146,6 @@ class MLAE:
         #Setting attributes
         self.restart()
 
-    def restart(self):
-        self.pdf_mks = None
-        self.list_of_circuits = []
-        self.list_of_jobs = []
-        self.theta = None
 
     @property
     def list_of_mks(self):
@@ -245,107 +165,7 @@ class MLAE:
             raise ValueError('For m_k only ints and list are aceptable types')
         #We update the allocate classical bits each time we change cbits_number
 
-    def apply_gate(self, m_k):
-        """
-        This method apply the self.q_gate to the self.q_prog an input
-        number of times, creates the correspondient circuit and job,
-        submit the job an get the results for an input number of shots.
-
-        Parameters
-        ----------
-        m_k : int
-            number of times to apply the self.q_gate to the quantum circuit
-
-        Returns
-        ----------
-
-        circuit : QLM circuit object
-            circuit object generated for the quantum program
-        job : QLM job object
-        """
-        circuit, job = apply_gate(
-            self.q_prog,
-            self.q_gate,
-            m_k,
-            nbshots=self.nbshots
-        )
-        return circuit, job
-
-    def result_processing(self, result_, mk):
-        """
-        This method receives a QLM Result object and proccess it in a
-        propper way for posterior applying of maximum likelihood algorithm
-
-        Parameters
-        ----------
-
-        result : QLM result object
-        mk : int
-            number of times Grover-like operator was applied for the
-            input result
-        
-        Returns
-        ----------
-
-        pdf : pandas dataframe
-            DataFrame with the results properly formated for maximum
-            likelihood algorithm
-
-        """
-        #result_ = run_job(result)
-        pdf_ = postprocess_results(result_)
-        #Change the result presentation
-        pdf = get_probabilities(pdf_)
-        if self.nbshots == 0:
-            #In this case QLM give simulated probabilities so we fixed
-            #to self.default_nbshots
-            n_k = self.default_nbshots
-        else:
-            #In this case we use the proper number of total measurements
-            n_k = self.nbshots
-        pdf['h_k'] = round(
-            pdf['Probability_|1>']*n_k, 0
-        ).astype(int)
-        pdf['n_k'] = n_k
-        pdf['m_k'] = mk
-        return pdf
-
-    def run_jobs(self, list_of_jobs, list_of_mks):
-        """
-        This functions submits a list of QLM jobs and get the result of
-        the simulation in the propper way for maximum likelihood
-        calculations
-
-        Parameters
-        ----------
-        
-        list_of_jobs : list
-            list of jobs for executing
-        list_of_mks : list
-            list with the number of applications (m_k) of the Grover-
-            like operator self.q_gate on the Quantum Program self.q_prog
-            Should be consistent with the list of jobs
-
-        Returns
-        ----------
-
-        pdf_mks : pandas dataframe
-            results of the measurement of the last qbit
-        """
-        #Complete Job Submision
-
-        batch_result = self.linalg_qpu.submit(Batch(list_of_jobs))
-        results = run_job(batch_result)
-
-        pdf_list = []
-        for result, m_k in zip(results.results, list_of_mks):
-            pdf_list.append(self.result_processing(result, m_k))
-
-        pdf_mks = pd.concat(pdf_list)
-        pdf_mks.reset_index(drop=True, inplace=True)
-            
-        return pdf_mks
-    
+ 
     def run_step(self, m_k):
         """
         This method applies the Grover-like operator self.q_gate to the
@@ -365,12 +185,14 @@ class MLAE:
             circuit object generated for the quantum program
         job : QLM job object
         """
+        routine = qlm.QRoutine()
+        register = routine.new_wires(self.oracle.arity)
+        routine.apply(self.oracle,register)
+        for i in range(m_k):
+            routine.apply(self.grover_oracle,register)
+        result, circuit, _, job = get_results(routine,linalg_qpu = self.linalg_qpu,shots = self.shots,qubits = self.index)
 
-        #First create the complete circuit and job
-        circuit, job = self.apply_gate(m_k)
-        #Then execute the job and get the results
-        pdf_mks = self.run_jobs([job], [m_k])
-        return pdf_mks, circuit, job
+        return result["Probability"]
 
 
     def run_mlae(self):#, list_of_mks=None, nbshots=None):
@@ -392,16 +214,8 @@ class MLAE:
 
         self.restart()
         #Clean the list in each run
-        pdf_list = []
         for m_k in self.list_of_mks:
-            step_circuit, step_job = self.apply_gate(m_k)
-            self.list_of_circuits.append(step_circuit)
-            self.list_of_jobs.append(step_job)
-        #Then execute the job and get the results
-        self.pdf_mks = self.run_jobs(
-            self.list_of_jobs,
-            self.list_of_mks
-        )
+            self.run_step(m_k)
 
         self.theta = self.launch_optimizer(self.pdf_mks)
 
