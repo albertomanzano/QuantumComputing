d3be7a241fe6da675a31cbefc124a7406d47355f
Alberto Manzano <alberto.manzano.herrero@gmail.com>
2022-03-15 18:25:37 +0000

    New version of maximum likelihood, further changes to be done

--- libraries/AE/maximum_likelihood_ae.py
+++ libraries/AE/maximum_likelihood_ae.py
@@ -19,6 +19,9 @@ Author:Gonzalo Ferro Costas
 MyQLM version:
 
 """
+import sys
+sys.path.append("../../")
+
 import copy
 import numpy as np
 import pandas as pd
@@ -29,47 +32,10 @@ from qat.qpus import get_default_qpu
 
 from AE.amplitude_amplification import grover
 from data_extracting import get_results
+from libraries.utils import bitfield_to_int
+from copy import deepcopy
 
 
-def likelihood(theta, m_k, h_k, n_k):
-    """
-    Calculates Likelihood from Suzuki papper. For h_k positive events
-    of n_k total events this function calculates the probability of
-    this taking into account that the probability of a positive
-    event is given by theta and by m_k
-    The idea is use this function to minimize it for this reason it gives
-    minus Likelihood
-
-    Parameters
-    ----------
-
-    theta : float
-        Angle (radians) for calculating the probability of measure a
-        positive event.
-    m_k : pandas Series
-        For MLQAE this a pandas Series where each row is the number of
-        times the operator Q was applied.
-        We needed for calculating the probability of a positive event
-        for eack posible m_k value: sin((2*m_k+1)theta)**2.
-    h_k : pandas Series
-        Pandas Series where each row is the number of positive events
-        measured for each m_k
-    n_k : pandas Series
-        Pandas Series where each row is the number of total events
-        measured for each m_k
-
-    Returns
-    ----------
-
-    float
-        Gives the -Likelihood of the inputs
-
-    """
-    theta_ = (2*m_k+1)*theta
-    first_term = 2*h_k*np.log(np.abs(np.sin(theta_)))
-    second_term = 2*(n_k-h_k)*np.log(np.abs(np.cos(theta_)))
-    l_k = first_term + second_term
-    return -np.sum(l_k)
 
 class MLAE:
     """
@@ -86,7 +52,7 @@ class MLAE:
         ----------
         
         kwars : dictionary
-            dictionary that allows the configuration of the ML-QPE algorithm:
+            dictionary that allows the configuration of the MLAE algorithm:
             Implemented keys:
             oracle: QLM gate
                 QLM gate with the Oracle for implementing the
@@ -99,74 +65,73 @@ class MLAE:
                 will be computed.
             qpu : QLM solver
                 solver for simulating the resulting circutis
-            delta : float
-                For avoiding problems when calculating the domain for theta
-            default_nbshots : int
-                default number of measurements for computing freqcuencies
-                when nbshots for quantum job is 0
-            iterations : int
-                number of iterations of the optimizer
-            display : bool
-                for displaying additional information in the optimization step
-            initial_state : QLM Program
-                QLM Program withe the initial Psi state over the
-                Grover-like operator will be applied
-                Only used if oracle is None
         """
         #Setting attributes
-        self.oracle = copy.deepcopy(oracle)
+        self.grover_oracle = None
         self.target = target
-        self.index = index
-            
-        #Creates the Grover-like operator from oracle
-        self.grover_oracle = grover(self.oracle,target,index)
+        self.index = index    
+        self.oracle = oracle
 
-
-        #A complete list of m_k
-        self.list_of_mks_ = kwargs.get('list_of_mks', 10)
-        self.list_of_mks = self.list_of_mks_
-
-        #If 0 we compute the exact probabilities
-        self.shots = kwargs.get('nbshots', 0)
         #Set the QPU to use
         self.linalg_qpu = kwargs.get('qpu')
         if (self.linalg_qpu is None):
-            self.linalg_qpu = get_default_qpu
-        ##delta for avoid problems in 0 and pi/2 theta limits
-        self.delta = kwargs.get('delta', 1.0e-5)
-        #This is the default number of shots used for computing
-        #the freqcuencies of the results when the computed probabilities
-        #instead of freqcuencies are provided (nbshots = 0 when qlm job
-        #is created)
-        self.default_nbshots = kwargs.get('default_nbshots', 100)
-        #number of iterations for optimization of Likelihood
-        self.iterations = kwargs.get('iterations', 100)
-        #For displaying extra info for optimization proccess
-        self.disp = kwargs.get('disp', True)
-        #Setting attributes
-        self.restart()
+            self.linalg_qpu = get_default_qpu()
+
+        # The schedule of the method
+        self.m_k = kwargs.get('m_k', [10])
+        self.n_k = kwargs.get('n_k',[100]*len(self.m_k))
+        self.h_k = [0]*len(self.m_k)
+        self.compute_h_k()
+
+        # Optimization
+        self.optimizer = kwargs.get('optimizer',\
+                lambda x: so.brute(func = x,ranges = [(0,np.pi/2)]))
 
 
     @property
-    def list_of_mks(self):
-        return self.list_of_mks_
+    def oracle(self):
+        return self._oracle
 
-    @list_of_mks.setter
-    def list_of_mks(self, value):
+    @oracle.setter
+    def oracle(self,value):
+        self._oracle = deepcopy(value) 
+        self.grover_oracle = grover(self.oracle,self.target,self.index)
+    
+    @property
+    def m_k(self):
+        return self._m_k
+
+    @m_k.setter
+    def m_k(self, value):
         if type(value) in [list, int]:
             if type(value) in [int]:
-                self.list_of_mks_ = list(range(value))
-                self.list_of_mks_.reverse()
+                self._m_k = list(range(value))
+                self._m_k.reverse()
             else:
-                self.list_of_mks_ = value
-            print('list_of_mks: {}'.format(self.list_of_mks))
-
+                self._m_k = value
         else:
             raise ValueError('For m_k only ints and list are aceptable types')
         #We update the allocate classical bits each time we change cbits_number
+    
+    @property
+    def n_k(self):
+        return self._n_k
+
+    @n_k.setter
+    def n_k(self,value):
+        if (type(value)==int) and (value>0):
+            self._n_k = [value]*len(self.m_k)
+        elif (type(value)==list):
+            if (len(value)==len(self.m_k)):
+                self._n_k = value
+            else:
+                raise ValueError("Sizes of m_k and nbshots must match. Otherwise pass an int")
+        else:
+            self._n_k = [100]*len(self.m_k)
+        
 
  
-    def run_step(self, m_k):
+    def run_step(self,m_k: int,n_k: int) -> int:
         """
         This method applies the Grover-like operator self.q_gate to the
         Quantum Program self.q_prog a given number of times m_k.
@@ -190,37 +155,59 @@ class MLAE:
         routine.apply(self.oracle,register)
         for i in range(m_k):
             routine.apply(self.grover_oracle,register)
-        result, circuit, _, job = get_results(routine,linalg_qpu = self.linalg_qpu,shots = self.shots,qubits = self.index)
+        result, circuit, _, job = get_results(routine,linalg_qpu = self.linalg_qpu,shots = n_k,qubits = self.index)
+        h_k = int(result["Probability"].iloc[bitfield_to_int(self.target)]*n_k)
 
-        return result["Probability"]
+        return h_k
 
 
-    def run_mlae(self):#, list_of_mks=None, nbshots=None):
+    @staticmethod
+    def likelihood(theta: float, m_k: int, n_k: int,h_k: int)->float:
         """
-        This method is the core of the Maximum Likelihood Amplitude
-        Estimation. It runs several quantum circuits each one increasing
-        the number of self.q_gate applied to the the initial self.q_prog
-    
+        Calculates Likelihood from Suzuki papper. For h_k positive events
+        of n_k total events this function calculates the probability of
+        this taking into account that the probability of a positive
+        event is given by theta and by m_k
+        The idea is use this function to minimize it for this reason it gives
+        minus Likelihood
+
         Parameters
         ----------
-        
-        list_of_mks : list (corresponding property will be overwrite)
-            python list with the different m_ks for executing the algortihm
-        nbshots : int (corresponding property will be overwrite)
-            number of shots for quantum job. If 0 exact probabilities
-            will be computed
+
+        theta : float
+            Angle (radians) for calculating the probability of measure a
+            positive event.
+        m_k : pandas Series
+            For MLQAE this a pandas Series where each row is the number of
+            times the operator Q was applied.
+            We needed for calculating the probability of a positive event
+            for eack posible m_k value: sin((2*m_k+1)theta)**2.
+        h_k : pandas Series
+            Pandas Series where each row is the number of positive events
+            measured for each m_k
+        n_k : pandas Series
+            Pandas Series where each row is the number of total events
+            measured for each m_k
+
+        Returns
+        ----------
+
+        float
+            Gives the -Likelihood of the inputs
 
         """
+        with np.errstate(divide='ignore'):
+            theta_ = (2*m_k+1)*theta
+            first_term = 2*h_k*np.log(np.abs(np.sin(theta_)))
+            second_term = 2*(n_k-h_k)*np.log(np.abs(np.cos(theta_)))
+            l_k = first_term + second_term
+        return l_k
 
-        self.restart()
-        #Clean the list in each run
-        for m_k in self.list_of_mks:
-            self.run_step(m_k)
+    def compute_h_k(self):
+        for i in range(len(self.m_k)):
+            self.h_k[i] = self.run_step(self.m_k[i],self.n_k[i])
 
-        self.theta = self.launch_optimizer(self.pdf_mks)
-
-
-    def launch_likelihood(self, pdf_input, N=100):
+    def cost_function(self,angle: float)->float:
         """
         This method calculates the Likelihood for theta between [0, pi/2]
         for an input pandas DataFrame.
@@ -247,23 +234,13 @@ class MLAE:
             l_k : likelihood for each theta
 
         """
-        pdf = pdf_input.copy(deep=True)
-        if pdf is None:
-            print(
-                """
-                Can not calculate Likelihood because pdf_input is empty.
-                Please provide a valida DataFrame.
-                """)
-            return None
-        theta = np.linspace(0+self.delta, 0.5*np.pi-self.delta, N)
-        m_k = pdf['m_k']
-        h_k = pdf['h_k']
-        n_k = pdf['n_k']
-        l_k = np.array([likelihood(t, m_k, h_k, n_k) for t in theta])
-        y_ = pd.DataFrame({'theta': theta, 'l_k': l_k})
-        return y_
+        cost = 0
+        for i in range(len(self.m_k)):
+            cost+=MLAE.likelihood(angle,self.m_k[i],self.n_k[i],self.h_k[i])
 
-    def launch_optimizer(self, results):
+        return -cost
+    
+    def optimize(self)->float:
         """
         This functions execute a brute force optimization of the
         likelihood function for an input results pdf.
@@ -285,15 +262,5 @@ class MLAE:
         optimum_theta : float
             theta  that minimize likelihood
         """
-
-        theta_domain = (0+self.delta, 0.5*np.pi-self.delta)
-        optimizer = so.brute(
-            likelihood,
-            [theta_domain],
-            (results['m_k'], results['h_k'], results['n_k']),
-            self.iterations,
-            disp=self.disp
-        )
-        optimum_theta = optimizer[0]
-        return optimum_theta
-
+        result = self.optimizer(self.cost_function)
+        return result[0]
