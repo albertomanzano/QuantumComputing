610c148fa7696411603e8060a533b8cf146be016
Alberto Manzano <alberto.manzano.herrero@gmail.com>
2022-03-18 10:35:24 +0000

    get results updated to require qpu

--- libraries/data_extracting.py
+++ libraries/data_extracting.py
@@ -13,13 +13,13 @@ Authors: Alberto Pedro Manzano Herrero & Gonzalo Ferro Costas
 import qat.lang.AQASM as qlm
 from qat.core import Result
 from qat.qpus import get_default_qpu
-from libraries.utils import bitfield
+from libraries.utils import bitfield, check_list_type
 import numpy as np
 import pandas as pd
 pd.options.display.float_format = '{:.6f}'.format
 np.set_printoptions(suppress=True)
 
-def get_results(quantum_object, linalg_qpu = None, shots:int = 0, qubits: list = None):
+def get_results(quantum_object, linalg_qpu, shots:int = 0, qubits: list = None):
     """
     Function for testing an input gate. This fucntion creates the
     quantum program for an input gate, the correspondent circuit
@@ -46,14 +46,18 @@ def get_results(quantum_object, linalg_qpu = None, shots:int = 0, qubits: list =
 
     """
     arity = quantum_object.arity
+    
+    if qubits is None:
+        qubits = np.arange(arity,dtype = int)
+    else:
+        qubits = check_list_type(qubits,int)
+
     if type(quantum_object) == qlm.Program:
         q_prog = deepcopy(quantum_object)
     else:
         q_prog = qlm.Program()
         qbits = q_prog.qalloc(arity)
         q_prog.apply(quantum_object, qbits)
-    if linalg_qpu is None:
-        linalg_qpu = get_default_qpu()
 
     circuit = q_prog.to_circ(submatrices_only=True)
     job = circuit.to_job(nbshots=shots, qubits=qubits)
@@ -66,14 +70,14 @@ def get_results(quantum_object, linalg_qpu = None, shots:int = 0, qubits: list =
     states = []
     list_int = []
     list_int_lsb = []
-    for i in range(2**arity):
-        reversed_i = int('{:0{width}b}'.format(i, width=arity)[::-1],2)
+    for i in range(2**qubits.size):
+        reversed_i = int('{:0{width}b}'.format(i, width=qubits.size)[::-1],2)
         list_int.append(reversed_i)
         list_int_lsb.append(i)
-        states.append("|"+ bin(i)[2:].zfill(arity)+">")
+        states.append("|"+ bin(i)[2:].zfill(qubits.size)+">")
 
-    probability = np.zeros(2**arity)
-    amplitude = np.zeros(2**arity)
+    probability = np.zeros(2**qubits.size)
+    amplitude = np.zeros(2**qubits.size)
     for samples in result:
         probability[samples.state.lsb_int] = samples.probability
         amplitude[samples.state.lsb_int] = samples.amplitude
